c907ef4fe0e1820e1cf24652f74ddaa51e52e716
Codidodido <codidodido@gmail.com>
2023-10-01 14:44:14 +0330

    Upload on github

---
+++ client.py
@@ -0,0 +1,78 @@
+import socket
+import threading
+from tkinter import *
+from tkinter import ttk
+from tkinter import simpledialog  # Import simpledialog for asking username
+
+def send_message():
+    message = message_input.get()
+    if message:
+        formatted_message = "-> " + username + ": " + message
+        message_text.configure(state="normal")
+        message_text.insert(END, formatted_message + '\n', 'user_message')  # Display the user message in light green
+        client_socket.send(formatted_message.encode('utf-8'))
+        message_input.delete(0, END)  # Clear the message input field
+        message_text.see(END)  # Auto-scroll to the end
+        message_text.configure(state="disabled")
+
+def receive_message():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            message_text.configure(state="normal")
+            if message.startswith("-> " + username):
+                # User's own message, highlight in light green
+                message_text.insert(END, message + '\n', 'user_message')
+            else:
+                # Other users' messages, highlight in dark green
+                message_text.insert(END, message + '\n', 'other_message')
+            message_text.see(END)  # Auto-scroll to the end
+            message_text.configure(state="disabled")
+        except:
+            # Handle any errors here
+            break
+
+# Create a pop-up dialog to get the username
+root = Tk()
+root.withdraw()  # Hide the root window
+username = simpledialog.askstring("Username", "Enter your username:")
+
+# Create the main chat window
+win = Tk()
+win.title("Codidodido Chat")
+
+greeting_text = ttk.Label(win, text=f"Welcome {username}")
+greeting_text.pack()
+
+message_text = Text(win, width=50, height=20, wrap=WORD, state="disabled")
+message_text.pack()
+
+# Define text tags for user's messages and other messages
+message_text.tag_configure('user_message', foreground='green')
+message_text.tag_configure('other_message', foreground='blue')
+
+message_box = LabelFrame(win, text=f"{username} Message: ")
+message_box.pack()
+
+message_input = ttk.Entry(message_box, width=40)
+message_input.grid(row=0, column=0)
+
+send_button = ttk.Button(message_box, text="Send", command=send_message)
+send_button.grid(row=0, column=1)
+
+# Create a socket for the client
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define the server address and port to connect to
+server_address = ('127.0.0.1', 12345)  # Replace 'server_ip_address' with the server's IP
+
+# Connect to the server
+client_socket.connect(server_address)
+
+# Create threads for sending and receiving messages
+receive_thread = threading.Thread(target=receive_message, daemon=True)
+
+# Start the receiving thread
+receive_thread.start()
+
+win.mainloop()
---
+++ server.py
@@ -0,0 +1,78 @@
+import socket
+import threading
+
+# Create a socket for the server
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define the server address and port
+server_address = ('0.0.0.0', 12345)  # Use 0.0.0.0 to listen on all available network interfaces
+
+# Bind the socket to the server address and port
+server_socket.bind(server_address)
+
+# Listen for incoming connections
+server_socket.listen(5)
+print("Server is listening for connections...")
+
+# List to store client connections
+clients = []
+
+# Function to broadcast messages to all connected clients
+def broadcast(message, client_socket):
+    for client in clients:
+        # Send the message to all clients except the sender
+        if client != client_socket:
+            try:
+                client.send(message)
+            except:
+                # Remove the client if unable to send the message
+                clients.remove(client)
+
+# Function to handle client connections
+def handle_client(client_socket):
+    while True:
+        try:
+            # Receive data from the client
+            message = client_socket.recv(1024)
+            if not message:
+                break
+            # Broadcast the received message to all clients
+            broadcast(message, client_socket)
+        except:
+            # Remove the client if there's an error
+            clients.remove(client_socket)
+            break
+
+# Function to send a message to all connected clients
+def send_to_clients(message):
+    message = message.encode('utf-8')
+    for client in clients:
+        try:
+            client.send(message)
+        except:
+            # Remove the client if unable to send the message
+            clients.remove(client)
+
+# Function for handling server-initiated messages
+def server_message_handler():
+    while True:
+        message = input("-> Server message: ")
+        send_to_clients(message)
+
+# Create a thread for handling server-initiated messages
+message_thread = threading.Thread(target=server_message_handler)
+message_thread.start()
+# Accept and handle incoming connections
+while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection from {client_address}")
+    # Add the client to the list
+    clients.append(client_socket)
+    
+    # Create a thread to handle the client's messages
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
+    
+    # Send a welcome message to the newly connected client
+    welcome_message = f"Welcome {client_address}!"
+    send_to_clients(welcome_message)
